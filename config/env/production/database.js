352daeadd4e28bfecb7f4152ad782b0a1f35ab19
AbdulrehmanTs <rohi644@gmail.com>
2022-06-22 21:04:22 +0500

    pool added

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -14,6 +14,14 @@ module.exports = ({ env }) => ({
         rejectUnauthorized: false
       },
     },
+    pool: {
+      min: 0,
+      max: 10,
+      idleTimeoutMillis: 30000000,
+      createTimeoutMillis: 30000000,
+      acquireTimeoutMillis: 30000000,
+      propagateCreateError: false,
+    },
     debug: false,
   },
 });
