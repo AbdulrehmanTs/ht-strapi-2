78e0304c16a15f4de38e8ebdb92506630856a174
AbdulrehmanTs <rohi644@gmail.com>
2022-06-22 15:26:05 +0500

    upload provider added

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,18 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'media-src': ["'self'", "'data:'", 'blob:', 'h-t-strapi.s3.amazonaws.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'h-t-strapi.s3.amazonaws.com'],
+        },
+      }
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
